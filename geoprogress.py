261dadebb7608ec91f3b4d45f019c91cf659ee0d
Kuga09 <glal64@tutamail.com>
2024-10-16 15:05:33 +0300

    1

--- geoprogress.py
+++ geoprogress.py
@@ -4,7 +4,9 @@ a=a.split()
 b=int(a[0])
 q=int(a[1])
 n=int(a[2])
-s=(b*(q**n-1))/(q-1)
+s=0
+for i in range(0,n):
+    s+=b*(q**i)
 output_data = open('output.txt','w')
 output_data.write(str(s))
 input_data.close()
